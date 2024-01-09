6d59976ff8e471bcd5d784beb8281673f837c6f9
salim <boughanmi1salim@gmail.com>
2024-01-09 22:49:54 +0100

    refactor service

--- store_application/lib/helper/api.dart
+++ store_application/lib/helper/api.dart
@@ -1,3 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class Api {
-  Future<dynamic> get({required String url}) async {}
+  Future<dynamic> get({required String url}) async {
+    http.Response response = await http.get(
+      Uri.parse(url),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+
+      return data;
+    } else {
+      throw Exception(
+          ' "there is problem with status code ${response.statusCode}');
+    }
+  }
 }
