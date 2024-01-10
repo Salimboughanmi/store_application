c9e27f911ddd8ccc3040f43eb75fa602f042287b
salim <boughanmi1salim@gmail.com>
2024-01-10 22:19:50 +0100

    refactor service3

--- store_application/lib/services/get_all_product-services.dart
+++ store_application/lib/services/get_all_product-services.dart
@@ -1,16 +1,12 @@
-import 'dart:convert';
-
-import 'package:http/http.dart';
 import 'package:store_application/helper/api.dart';
 import 'package:store_application/models/product_models.dart';
 
 class GetAllProductService {
   Future<List<ProductModels>> getAllProduct() async {
-    Response response =
+    List<dynamic> data =
         await Api().get(url: "https://fakestoreapi.com/products");
 
-    List<dynamic> data =
-        jsonDecode(response.body); // jsonDecode convertir string vers list
+    // jsonDecode convertir string vers list
 
     List<ProductModels> productsList = [];
     for (int i = 0; i < data.length; i++) {
