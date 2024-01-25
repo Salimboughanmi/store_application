f1708f8c5e59fe22dcfd028613aa6ed68153bea4
salim <boughanmi1salim@gmail.com>
2024-01-25 21:36:07 +0100

    add routes

--- store_application/lib/screens/home_page.dart
+++ store_application/lib/screens/home_page.dart
@@ -17,6 +17,51 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         centerTitle: true,
       ),
+      body: Stack(
+        //stack mettre les widgets l'un sur l'autre
+        children: [
+          Container(
+            height: 130,
+            width: 180,
+            decoration: BoxDecoration(boxShadow: [
+              BoxShadow(
+                blurRadius: 40,
+                color: const Color.fromARGB(255, 196, 204, 196),
+                spreadRadius: 0,
+                offset: Offset(10, 10),
+              ),
+            ]),
+            child: const Card(
+              elevation: 10,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "HandBag slk",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          r"$100",
+                        ),
+                        Icon(Icons.favorite)
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            child: Text("salim boughanmi"),
+          )
+        ],
+      ),
     );
   }
 }
